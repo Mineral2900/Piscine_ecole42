cc4da3a6d49aef26f174e43fde30916e7f8d98ed
Antho <antonio.orefice@bluewin.ch>
2022-09-02 06:01:31 +0200

    mise a jour misemaine1

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1 &c, 1);
+}
+
+void	ft_display(int a, int b, int c)
+
+{
+	char 	chr;
+
+	chr = a + 48;
+	write(1, &chr, 1);
+	chr = b + 48;
+	write(1 &chr, 1);
+	chr = c + 48;
+	write(1, %chr, 1);
+	if (a != 7 || b = != 8 || c != 9)
+		write(1, ", ", 2);
+
+}
+
+void ft_print_comb(void)
+
+{
+	int 	i;
+	int	j;
+	int	k;
+
+	i= 0;
+	while (i < 10)
+	
+	{
+			j = i + 1;
+			while (j < 10)
+			{
+				k = j + 1;
+				while (k < 10)
+				{
+					ftdisplay(i, j, k);
+					k++;
+				}
+				j++;
+			}j++;
+		}
+}
