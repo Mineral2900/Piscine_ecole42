cc0e6159f407b84bff045f1acfeb7211aa8182c4
Antho <antonio.orefice@bluewin.ch>
2022-09-03 16:33:20 +0200

    rush01 test

--- Rush00/excomm/rush00c.c
+++ Rush00/excomm/rush00c.c
@@ -10,13 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+void ft_putchar(char c);
 
-int rush00c()
+void middle(int x, int y)
 
 {
-	x = 5
-	y = 5
-	rush00c(5, 5);
-	return (0);
+	int i;
+	int count;
+
+	if (y > 1)
+	{
+		count = 0;
+		while (count < (y - 2))
+		{
+			ft_putchar('\n');
+			ft_putchar('*');
+			i = 0;
+			while (i < (x - 2))
+			{
+				ft_putchar(' ');
+				++i;
+
+			}
+			if (x > 1)
+				ft_putchar('*');
+			++count;
+		}
+	}
 }
